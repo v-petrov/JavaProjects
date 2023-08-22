e12ce4b4d153edbe7233e00814828e400e271417
v-petrov <vapetrov@tu-sofia.bg>
2023-08-22 19:26:11 +0300

    delete a node without having the head

--- Tasks/Solution.java
+++ Tasks/Solution.java
@@ -30,6 +30,10 @@ class Solution {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         System.out.println(searchMatrix(matrix, 13));
     }
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
     public int passThePillow(int n, int time) {
         int k = time / (n - 1);
         if (k == 0) {

0fdfaad8fefa5a0a956ff344910dbd57f78fefcb
v-petrov <vapetrov@tu-sofia.bg>
2023-08-27 12:46:03 +0300

    length of last word

--- Tasks/Solution.java
+++ Tasks/Solution.java
@@ -30,6 +30,19 @@ class Solution {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         System.out.println(searchMatrix(matrix, 13));
     }
+    public int lengthOfLastWord(String s) {
+        int cnt = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != 32) {
+                cnt++;
+            } else {
+                if (cnt != 0) {
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;

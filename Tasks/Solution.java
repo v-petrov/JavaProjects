77038b19b61c8a5165d9b5883cc782248bcb591c
v-petrov <vapetrov@tu-sofia.bg>
2023-10-13 17:19:25 +0300

    minCost

--- Tasks/Solution.java
+++ Tasks/Solution.java
@@ -34,6 +34,22 @@ class Solution {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         System.out.println(searchMatrix(matrix, 13));
     }
+    public int minCostClimbingStairs(int[] cost) {
+        int j;
+        int[] dp = new int[cost.length + 1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < cost.length + 1; i++) {
+            if (i == cost.length) {
+                j = 0;
+            } else {
+                j = cost[i];
+            }
+            dp[i] = Math.min(j + dp[i - 1], j + dp[i - 2]);
+        }
+        return dp[cost.length];
+    }
     public TreeNode bstToGst(TreeNode root) {
         rightSubTree1(root);
         return root;

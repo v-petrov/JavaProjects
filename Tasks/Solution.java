b74897e1a937a0d07b8c7949f9194f7bdc0c6921
v-petrov <vapetrov@tu-sofia.bg>
2023-10-13 17:20:04 +0300

    customeSort

--- Tasks/Solution.java
+++ Tasks/Solution.java
@@ -34,6 +34,26 @@ class Solution {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         System.out.println(searchMatrix(matrix, 13));
     }
+    public String customSortString(String order, String s) {
+        StringBuilder bul = new StringBuilder();
+        int[] charsS = new int[26];
+        for (char c : s.toCharArray()) {
+            charsS[c - 'a']++;
+        }
+        for (char c : order.toCharArray()) {
+            while (charsS[c - 'a'] != 0) {
+                bul.append(c);
+                charsS[c - 'a']--;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            while (charsS[i] != 0) {
+                bul.append((char) (i + 'a'));
+                charsS[i]--;
+            }
+        }
+        return bul.toString();
+    }
     public int minCostClimbingStairs(int[] cost) {
         int j;
         int[] dp = new int[cost.length + 1];
